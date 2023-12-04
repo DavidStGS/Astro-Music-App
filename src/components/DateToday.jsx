b5508e85bbcf5a103a0d2b308e81d32befa030f8
DavidSGS <davidgarcia200124@gmail.com>
2023-12-04 04:04:16 -0500

    Complete

--- src/components/DateToday.jsx
+++ src/components/DateToday.jsx
@@ -1,15 +1,19 @@
+import { useEffect } from "react";
+
 export function DateToday () {
   const currentTime = new Date();
   const currentHour = currentTime.getHours();
 
   let greeting = "";
+useEffect(() => {
+    if (currentHour >= 8 && currentHour < 12) {
+      greeting = "¡Buenos días!";
+    } else if (currentHour >= 12 && currentHour < 20) {
+      greeting = "¡Buenas tardes!";
+    } else {
+      greeting = "¡Buenas noches!";
+    }
+}, [greeting]);
 
-  if (currentHour >= 8 && currentHour < 12) {
-    greeting = "¡Buenos días!";
-  } else if (currentHour >= 12 && currentHour < 20) {
-    greeting = "¡Buenas tardes!";
-  } else {
-    greeting = "¡Buenas noches!";
-  }
   return <p className="text-[32px] font-extrabold text-white tracking-tighter">{greeting}</p>;
 };
